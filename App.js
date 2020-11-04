37cad317a537f4a6027ddd728e6991d55dd30b92
Daniel Scholtus <sdeonline@gmail.com>
2020-11-03 22:25:30 -0300

    v1.0.1

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
-import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
+import React, { useRef, useEffect } from 'react'
+import { BackHandler } from 'react-native'
+import { WebView } from 'react-native-webview'
+import Constants from 'expo-constants'
 
-export default () => <WebView
-  source={{ uri: 'https://scholtus.olistshops.com/' }}
-  style={{ marginTop: Constants.statusBarHeight }}
-/>
+const App = () => {
 
+  const webviewRef = useRef(null)
+
+  useEffect(() => {
+    const backAction = () => {
+      if (this.canGoBack) {
+        this.webviewRef.goBack()
+        return true
+      }
+    }
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    )
+
+    return () => backHandler.remove()
+  }, [])
+
+  return (
+    <WebView
+      source={{ uri: 'https://scholtus.olistshops.com/' }}
+      style={{ marginTop: Constants.statusBarHeight }}
+      ref={this.webviewRef}
+      onNavigationStateChange={(navState) => {
+        this.canGoBack = navState.canGoBack
+      }}
+    />
+  )
+}
+
+export default App
