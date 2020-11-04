dd2c15fe1897663b9048bf5c563487b4b02e77e7
Daniel Scholtus <sdeonline@gmail.com>
2020-11-04 01:48:26 -0300

    v1.0.3

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import React, { useRef, useEffect } from 'react'
-import { BackHandler } from 'react-native'
+import { BackHandler, Text, ToastAndroid } from 'react-native'
 import { WebView } from 'react-native-webview'
 import Constants from 'expo-constants'
 
 const App = () => {
 
   const webviewRef = useRef(null)
+  const canGoBack = useRef(false)
+
+  const backAction = () => {
+    if (canGoBack.current) {
+      webviewRef.current.goBack()
+      return true
+    }
+  }
+
+  const handleNavigationStateChange = (navState) => {
+    canGoBack.current = navState.canGoBack
+  }
+
+  const handleMessage = ({ nativeEvent: state }) => {
+    if (state.data === "navigationStateChange") {
+      canGoBack.current = state.canGoBack
+    }
+  }
+
+  const injectedJavaScript = `
+          (function() {
+            function wrap(fn) {
+              return function wrapper() {
+                var res = fn.apply(this, arguments);
+                window.ReactNativeWebView.postMessage('navigationStateChange');
+                return res;
+              }
+            }
+
+            history.pushState = wrap(history.pushState);
+            history.replaceState = wrap(history.replaceState);
+            window.addEventListener('popstate', function() {
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+            });
+          })();
+
+          true;
+        `
 
   useEffect(() => {
-    const backAction = () => {
-      if (this.canGoBack) {
-        webviewRef.goBack()
-        return true
-      }
-    }
-
     const backHandler = BackHandler.addEventListener(
       "hardwareBackPress",
       backAction
@@ -28,9 +59,9 @@ const App = () => {
       source={{ uri: 'https://scholtus.olistshops.com/' }}
       style={{ marginTop: Constants.statusBarHeight }}
       ref={webviewRef}
-      onNavigationStateChange={(navState) => {
-        this.canGoBack = navState.canGoBack
-      }}
+      onNavigationStateChange={handleNavigationStateChange}
+      injectedJavaScript={injectedJavaScript}
+      onMessage={handleMessage}
     />
   )
 }
